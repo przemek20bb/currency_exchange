a00141e7a39fed33a3985fb3f4168afe4a22ebee
przemek20bb <przemyslawpasiek94@gmail.com>
2020-08-25 23:18:29 +0200

    Druga wersja kantora

--- Calculator.py
+++ Calculator.py
@@ -1,21 +1,23 @@
 import datetime
+from Currency_rate import reading_rate
 
 czas=datetime.datetime.now()
 actual_date=czas.strftime("%I:%M %d.%m.%Y %A")
 print ("DATA: " + actual_date)
-print("Hej, podaj aktualny kurs euro:")
-euro=input()
-#funt=input()
-#dolar=input()
-#frank=input()
+print ("Podaj nazwy walut dla ktorych chcesz sprawdzic kurs:")
+waluta=[]
+#aluta[0]=str(input())
+waluta1=input()
+waluta2=input()
+euro=reading_rate("kursy_walut.txt",waluta1)
+dolar=reading_rate("kursy_walut.txt",waluta2)
 
-print("Aktualny kurs euro wynosi:" + euro)
-#print("Aktualny kurs euro wynosi:" + dolar)
-#print("Aktualny kurs euro wynosi:" + frank)
-#print("Aktualny kurs euro wynosi:" + funt)
+print("Aktualny kurs euro wynosi:" + euro + "(sprzedaz)")
+print("Aktualny kurs dolara wynosi:" + dolar + "(sprzedaz)")
+
 
 print("Podaj Ile masz pieniążków: [zł]")
 oszczednosci=input()
-oszczednosci_w_euro=int (oszczednosci)/int (euro)
-print("Za swoje oszczednosci mozesz kupic " + str(int (oszczednosci_w_euro)) + " euro")
+#oszczednosci_w_euro=(oszczednosci)/(euro)
+#print("Za swoje oszczednosci mozesz kupic " + str(int (oszczednosci_w_euro)) + " euro")
 
