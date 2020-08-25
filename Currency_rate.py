ef4274df942c7905bb809ba411f71fe3916d12e6
przemek20bb <przemyslawpasiek94@gmail.com>
2020-08-25 23:19:37 +0200

    Druga wersja kantora

--- Currency_rate.py
+++ Currency_rate.py
@@ -2,23 +2,25 @@
 def reading_rate(sciezka,waluta):
     f = open(sciezka,mode="r+")
     lista1=f.readlines()
-    print(lista1)
+    #print(lista1)
     f.close()
     y=waluta
     m="<nazwa_waluty>"+(y)+"</nazwa_waluty>\n"
-    print(m)
+    #print(m)
     k=lista1.index(m)
 
     w=lista1[k+3]
     t=lista1[k+4]
-    print(w)
-    print(t)
+    #print(w)
+    #print(t)
 
     l= w[12:16].replace(",",".")
-    print ("Kurs kupna: "+ str (l))
+    #print ("Kurs kupna: "+ str (l))
 
     r= t[16:20].replace(",",".")
-    print ("Kurs sprzedazy: "+ str (r))
+    #print ("Kurs sprzedazy: "+ str (r))
     return r
-reading_rate("kursy_walut.txt","dolar")
+#reading_rate("kursy_walut.txt","frank szwajcarski")
+#frank=reading_rate("kursy_walut.txt","frank szwajcarski")
+#print(frank)
 # jak wczytac zmienna zwracana przez funkcje?
--- readme.txt
+++ readme.txt
@@ -1,5 +1,5 @@
 Calculator 1.0:
 #1.Wczytać aktualne kursy walut z http://nbp.pl/kursy/xml/LastA.xml do pliku
-#2.Pobranie z pliku kursu euro,,dolara,funta. 
+#2.Pobranie z pliku kursu euro,,dolara,funta. - done
 #3.przeliczenie ile dostane pieniędzy za sprzedanie walut (euro + funty? - należy wybrać jakie waluty chce sprzedać)
-#4? Rozbicie tego na kilka plików?
+#4? Rozbicie tego na kilka plików - done
