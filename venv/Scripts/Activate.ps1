c54db6eaf7a4c1f6850fcc6745d8f31f550a086b
przemek20bb <przemyslawpasiek94@gmail.com>
2021-01-12 22:19:01 +0100

    updating venv

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Przemek\Desktop\Python\Python_projects\kantor_online\venv"
+$env:VIRTUAL_ENV="C:\Users\Przemek\Desktop\currency_exchange\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Przemek\Desktop\Python\Python_projects\kantor_online\venv"
+set "VIRTUAL_ENV=C:\Users\Przemek\Desktop\currency_exchange\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
